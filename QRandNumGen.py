c4d9ca93a54845833937bc900816a00580b3b3e2
Agnij-Moitra <79319209+Agnij-Moitra@users.noreply.github.com>
2021-05-25 20:48:39 +0530

    Update QRandNumGen.py

--- QRandNumGen.py
+++ QRandNumGen.py
@@ -1,30 +1,22 @@
+#%%
 import qiskit as q
-from numpy import pi
-from qiskit import *
-
+import numpy as np
 bk = q.Aer.get_backend("qasm_simulator")
-
-
+#%%
 def main(bitstring):
     """
     bitsring is the number of digits that'll be generated
     """
-
+    li = np.arange(bitstring)
     # initializing qubits
-    qc = q.QuantumCircuit(bitstring + 1, bitstring + 1)
-
-    # applying h gate and measuring
-    for i in range(bitstring):
-        qc.rz(pi / 2, i)
-        qc.sxdg(i)
-        qc.rz(pi / 2, i)
-        qc.measure(i, i)
-
-    # executing circuit
-    result = q.execute(qc, bk, shots=1).result()
-
+    qc = q.QuantumCircuit(bitstring, bitstring)
+    # adding gates
+    qc.h(li)
+    # measuring     
+    qc.measure(li, li)
+    # get the bitstring
+    result = q.execute(qc, bk, shots = 1).result()
     # the bitstring
     counts = result.get_counts(qc)
-    ans = str(list(counts)[0])
-
-    return ans
+    return int(list(counts)[0])
+#%%
